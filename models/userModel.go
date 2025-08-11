a875d83b96b12399822d425a996b7eb0c8652588
Basil Okache <basilokache@gmail.com>
2025-08-12 00:01:03 +0100

    Error fix

--- models/userModel.go
+++ models/userModel.go
@@ -5,8 +5,8 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type User struct(
-	ID				primitive.ObjectID		`bson:" id"`
+type User struct{
+	ID				primitive.ObjectID		`bson:"_id"`
 	First_name		*string 				`json: "first_name" validate: "required, min=2, max=100"`
 	Last_name		*string 				`json: "last_name" validate: "required, min=2, max=100`
 	Password		*string					`json: "password validate: "required, min=6"`
@@ -18,4 +18,4 @@ type User struct(
 	Created_at		time.Time				`json: "created_at"`
 	Updated_at		time.Time				`json: "updated_at"`
 	User_id			string					`json: "user_id"` 
-)
+}
