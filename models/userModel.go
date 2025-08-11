7922d562e355cc327bd48435d9b11150cb1f8b52
Basil Okache <basilokache@gmail.com>
2025-08-11 23:56:35 +0100

    Updated user model to include validation

--- models/userModel.go
+++ models/userModel.go
@@ -1,20 +1,21 @@
 package models
 
-import(
-
+import (
+	"time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct(
-	ID
-	First_name
-	Last_name
-	Password
-	Email
-	Phone
-	Token
-	User_type
-	Refresh_token
-Created_at
-Updated_at
-User_id
+	ID				primitive.ObjectID		`bson:" id"`
+	First_name		*string 				`json: "first_name" validate: "required, min=2, max=100"`
+	Last_name		*string 				`json: "last_name" validate: "required, min=2, max=100`
+	Password		*string					`json: "password validate: "required, min=6"`
+	Email			*string					`json: "email" validate: "email, required"`
+	Phone			*string					`json: "phone" validate: "required"` 
+	Token			*string					`json: "token"` 
+	User_type		*string					`json: "user_type" validate: "required, eq=ADMIN|eq=USER"` 
+	Refresh_token	*string					`json: "refresh_token"`
+	Created_at		time.Time				`json: "created_at"`
+	Updated_at		time.Time				`json: "updated_at"`
+	User_id			string					`json: "user_id"` 
 )
